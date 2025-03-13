8acc90d101f5e41e0fe9f7b11a3f615a3ab6f1de
AKER <aker1746@gmail.com>
2025-03-13 01:31:59 -0700

    Adding Start

---
+++ Geminal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BLUE_BOLD="\e[1;34m"
+YELLOW_BOLD="\e[1;93m"
+RESET="\e[0m"
+RED="\e[1;91m"
+GREEN="\e[1;92m"
+WHITE="\e[1;97m"
+PINK="\e[1;95m"
+
+echo " "
+function funlogo(){
+    toilet -f pagga -F metal GeminaL
+}
+funlogo
+
+
+echo -e "${WHITE}Say...What's you ask? ${RESET}"
+read ask
+
+
+GEMINI_API_KEY="AIzaSyDiJA8LqKmLPMRqlJmDrHzvnqMcLVD1HBU"
+
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=GEMINI_API_KEY" \
+-H 'Content-Type: application/json' \
+-X POST \
+-d '{
+  "contents": [{
+    "parts":[{"text": "${ask}"}]
+    }]
+   }'
